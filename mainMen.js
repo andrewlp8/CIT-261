b80a3e0709f4941c6495bcec9aaecafd0bba9f85
Petersen-Andrew <Petersen-Andrew@users.noreply.github.com>
2018-07-17 13:19:15 -0700

    committ 07.17.18.01
    added sounds with controls

--- mainMen.js
+++ mainMen.js
@@ -1,6 +1,4 @@
 
-
-
 // about button
 function aboutInfo() {
     var modal = document.getElementById('about');
@@ -23,7 +21,6 @@ function aboutInfo() {
     }
     
 }
-
 
 // instructions button
 function instructions() {   
@@ -78,5 +75,65 @@ function toMap() {
 }
 
 function playAgain() {
-    window.location.assign("projIndex.html");
+    var mapScreen = document.getElementById("missionMapScreen"); 
+    var mMenuVH = document.getElementById("containerMainMenu"); 
+    mapScreen.style.display = "none";
+    mMenuVH.style.display = "block";
+    event.preventDefault();
+}
+
+// sound
+function clickSound() {
+    var clickS = document.getElementById('clickSound');
+
+    clickS.play();
+}
+
+// Music
+function musicON_OFF() {
+
+    var backMusic = document.getElementById("backMusic");
+        if (backMusic.paused) {
+            backMusic.play();
+        }
+        else {
+            backMusic.pause();
+        }
+
+}
+// for volume
+function volumeHigh() {
+    var vol = document.getElementById('backMusic');
+    vol.volume = 1.0;
+}
+
+function volumeMedium() {
+    var vol = document.getElementById('backMusic');
+    vol.volume = 0.5;
+}
+function volumeLow() {
+    var vol = document.getElementById('backMusic');
+    vol.volume = 0.2;
+}
+
+function musicOpts() {
+    var modal = document.getElementById('musicContainer');
+    var span = document.getElementsByClassName("close")[2];
+
+    modal.style.display = "block";
+    
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+       
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+           
+        }
+    }
 }
