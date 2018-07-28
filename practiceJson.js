164303f8a8c67877791341ac36a359ea070c4448
Petersen-Andrew <Petersen-Andrew@users.noreply.github.com>
2018-07-27 22:43:21 -0700

    testrun03

--- practiceJson.js
+++ practiceJson.js
@@ -2,15 +2,18 @@
 
 // load mission click spinning globe
 function initMap() {
-             getName();
-// declare min to max for rand num generator
-var min = 0;
-var max = 5;
+    function getName() {
+        var getGender = localStorage.getItem("gender");
+        var getFirst = localStorage.getItem("firstName");
+        var getLast = localStorage.getItem("lastName");
+    
+        var display = document.getElementById("tagName").innerHTML = "<h2>" + getGender + " " + getFirst + " " + getLast + "</h2>";
+    
+    
+        return display;
+    }
 
-
-
-var missionRandom = Math.floor(Math.random() * (max - min + 1) + min);
-var j = missionRandom;
+    getName();
 
 // Pull mission data from missions.json file here
 // AJAX Example
@@ -18,7 +21,9 @@ var myRequest = new XMLHttpRequest();
 myRequest.open('GET', 'https://raw.githubusercontent.com/Petersen-Andrew/CIT-261/master/missions.json');
 myRequest.onload = function () {
     var myData = JSON.parse(myRequest.responseText);
-    
+
+    var missionRandom = Math.floor(Math.random() * myData.missions.length);
+    var j = missionRandom;
 
     // name of location & info
     var name = (myData.missions[j].name);
@@ -31,34 +36,30 @@ myRequest.onload = function () {
     // Longitude
     var y = (myData.missions[j].y);
 
-    // for (var i = 0; i < myData.missions[1].key.length; i++) {
-    //     forDisplay += myData.vehicles[1].models[i] + "<br/>";
-        
-    // }
-        
+    // get location
+    var location = { lat: x, lng: y };
+    // The map, centered at location
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 10, center: location });
 
-    document.getElementById('testOut').innerHTML = j + "<h4>" + name + "</h4>" + info + x + "<br/>" + y;
-    
+    // The marker, positioned at location
+    var marker = new google.maps.Marker({ position: location, map: map });
+
+    var getFirst = localStorage.getItem("firstName");
+
+        document.getElementById("infoDiv").innerHTML = "Here is some more information about the <br>" + "<strong>" + name + '</strong>' + info + "<br><br/>";
+        document.getElementById("missionTitle").innerHTML = "Congratulations " + getFirst + "!<br/> You have landed on the <br/> "+ name;
 
 
 };
 
 myRequest.send();
 
-
-
-    document.getElementById("infoDiv").innerHTML = "You are going to the <br>" + "<strong>" + name + '</strong>' + info + "<br><br/>";
-    document.getElementById("missionTitle").innerHTML = name;
+    
 
 // https://www.latlong.net/
 // http://lifey.org/lds-missions/
 
 
-// get location
-var location = { lat: x, lng: y };
-// The map, centered at location
-var map = new google.maps.Map(
-    document.getElementById('map'), { zoom: 10, center: location });
-// The marker, positioned at location
-var marker = new google.maps.Marker({ position: location, map: map });
+
 }
